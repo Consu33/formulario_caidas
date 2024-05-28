ce7279319f19789668dd3cdba1f4a20b16b8dc9c
Consu33 <105826956+Consu33@users.noreply.github.com>
2024-05-28 15:20:42 -0400

    Update editar_datos.php

--- editar_datos.php
+++ editar_datos.php
@@ -1,1 +1,35 @@
+<?php
+//$servidor = "localhost";
+//$usuario = "root";
+//$clave = "";
+//$baseDeDatos = "formulariocaida";
+
+//$conexion = new mysqli($servidor, $usuario, $clave, $baseDeDatos);
+
+//if ($conexion->connect_error) {
+//    die("Conexión fallida: " . $conexion->connect_error);
+//}
+
+//$id = $_POST['id'];
+//$nombre = $_POST['nombre'];
+//$apellido = $_POST['apellido'];
+//$rut = $_POST['rut'];
+//$sexo = $_POST['sexo'];
+//$edad = $_POST['edad'];
+
+// Actualizar datos del paciente
+//$consulta = $conexion->prepare("UPDATE paciente SET nombre = ?, apellido = ?, rut = ?, sexo = ?, edad = ? WHERE id = ?");
+//$consulta->bind_param("ssssii", $nombre, $apellido, $rut, $sexo, $edad, $id);
+
+//if ($consulta->execute()) {
+//    echo "Datos actualizados correctamente.";
+//} else {
+//    echo "Error al actualizar datos: " . $consulta->error;
+//}
+
+//$consulta->close();
+//$conexion->close();
+?>
+
+
 
